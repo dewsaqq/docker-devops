1f3046df00fa7d0284c58854e8bcfef0b996cf36
arek-dabrowski <arekdbr97@gmail.com>
2020-04-09 17:29:56 +0200

    add gcd calculation to app

--- 03-multi-docker-app/backend/index.js
+++ 03-multi-docker-app/backend/index.js
@@ -31,6 +31,32 @@ app.get('/', (req, resp) => {
   resp.send('Hello world!');
 });
 
+app.get("/:num1/:num2", (req, resp) => {
+    const numberToGCD1 = req.params.num1;
+    const numberToGCD2 = req.params.num2;
+    const parsedCacheValue = numberToGCD1 + ',' + numberToGCD2;
+
+    console.log("New request with numbers: " + numberToGCD1 + " and " + numberToGCD2);
+
+    redisClient.get(parsedCacheValue, (err, cachedValue) => {
+        if (!cachedValue) {
+            const result = GCD(numberToGCD1, numberToGCD2);
+            redisClient.set(parsedCacheValue, parseInt(result));
+            resp.send('GCD of ' + numberToGCD1 + ' and ' + numberToGCD2 + ' is: ' + result);
+        } else {
+            resp.send('Cached GCD of ' + numberToGCD1 + ' and ' + numberToGCD2 + ' is: ' + cachedValue);
+        }
+    });
+});
+
+const GCD = (a, b) => {
+    if (!b) {
+        return a;
+    }
+
+    return GCD(b, a % b);
+}
+
 app.listen(8080, err => {
   console.log('Server listening on port 8080');
 });
